7eccb35644bcd96bb601e5d996099f3d9a1bf67e
Gadolfthewhite <32609678+Gadolfthewhite@users.noreply.github.com>
2020-10-06 09:01:13 +0800

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,35 +1,41 @@
+#coding:utf8
 import cv2 as cv
 import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-def rgb1gray(f, method='NTFC',showflag=True):
+
+def rgb1gray(f, method='NTSC', showflag=True):
     """
     Turn rgb img to gray img
     :param f: image path
-    :param method: 'average' or 'NTFC'(defaut)
+    :param method: 'average' or 'NTSC'(defaut)
     return gray image
     """
     if os.path.exists(f):
         rgbimg = cv.imread(f)
-        rgbimg=np.array(rgbimg,dtype=np.float32)
+        rgbimg = np.array(rgbimg, dtype=np.float32)
         if method == 'average':
             grayimg = (rgbimg[:, :, 0] + rgbimg[:, :, 1] + rgbimg[:, :, 2]) / 3
             grayimg = np.around(grayimg)
-            grayimg = np.array(grayimg,dtype=np.uint8)
+            grayimg = np.array(grayimg, dtype=np.uint8)
             if showflag:
                 plt.figure()
-                plt.imshow(grayimg,'gray')
+                plt.imshow(grayimg, 'gray')
+                plt.axis('off')
+                plt.savefig('figures/Average_gray%s.png' % (f.split('.')[0]), dpi=800, bbox_inches='tight')
                 plt.title('Average')
             return grayimg
-        elif method == 'NTFC':
+        elif method == 'NTSC':
             grayimg = (0.2989 * rgbimg[:, :, 0] + 0.5870 * rgbimg[:, :, 1] + 0.1140 * rgbimg[:, :, 2])
             grayimg = np.around(grayimg)
-            grayimg = np.array(grayimg,dtype=np.uint8)
+            grayimg = np.array(grayimg, dtype=np.uint8)
             if showflag:
                 plt.figure()
-                plt.imshow(grayimg,'gray')
-                plt.title('NTFC')
+                plt.imshow(grayimg, 'gray')
+                plt.axis('off')
+                plt.savefig('figures/NTSC_gray%s.png' % (f.split('.')[0]), dpi=800, bbox_inches='tight')
+                plt.title('NTSC')
             return grayimg
         else:
             print('The method parameter is invalid, please input again!')
